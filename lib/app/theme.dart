a0a116e1df715efed15b1f0fb1c33eb0ee6a4ea0
thisen <thiseansandeepa14@gmail.com>
2026-02-16 23:17:22 +0530

    create theme dart

---
+++ lib/app/theme.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+final ThemeData appTheme = ThemeData(
+  useMaterial3: true,
+  fontFamily: 'Roboto',
+  colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFF2563EB)),
+  scaffoldBackgroundColor: Colors.white,
+);
