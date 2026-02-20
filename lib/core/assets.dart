16d26a3d6cf79d292e33a7235c90057e960eda8d
ime20041126 <imesha.20240948@iit.ac.lk>
2026-02-20 15:16:55 +0530

    Create assets.dart

---
+++ lib/core/assets.dart
@@ -0,0 +1,17 @@
+class AppAssets {
+  static const String welcomeLogo = 'assets/images/welcome_screen_logo.png';
+  static const String welcomePage = 'assets/images/welcome_page.png';
+  static const String createProfilePage =
+      'assets/images/create_your_profile_page.png';
+  // Placeholder profile image used in CreateProfileScreen
+  static const String demoProfile = 'assets/images/welcome_screen_logo.png';
+  // Alternate name for create-profile placeholder photo
+  static const String createProfilePhoto =
+      'assets/images/create_your_profile_page.png';
+  // Backwards-compatible alias expected by some screens
+  static const String profilePhoto = createProfilePhoto;
+  static const String workerVerificationPage1 =
+      'assets/images/worker_verification_page1.png';
+  static const String workerVerificationPage2 =
+      'assets/images/worker_verification_page2.png';
+}
