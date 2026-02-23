01959a8fb11e93bacd99e039185c1f1aa8183a8e
thisen <thiseansandeepa14@gmail.com>
2026-02-23 12:21:59 +0530

    updated main dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'app/routes.dart';
+import 'app/theme.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,6 +13,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      title: 'TECHNI Worker',
+      theme: appTheme,
       initialRoute: '/',
       routes: appRoutes,
     );
